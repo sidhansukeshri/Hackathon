62e2ad98155602ff24853930b9095d560298074f
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2023-04-27 12:06:41 +0530

    Update python code for searching the data from sql and displaying user interest data .py

--- python code for searching the data from sql and displaying user interest data .py
+++ python code for searching the data from sql and displaying user interest data .py
@@ -1,55 +1,67 @@
 import mysql.connector
 
-# Connect to the database
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Spysiddhu@#2004",
-    database="scrap"
-)
+class Database:
+    def __init__(self, host, user, password, database):
+        self.host = "localhost"
+        self.user = "root"
+        self.password = "Spysiddhu@#2004"
+        self.database = "scrap"
+        self.connect()
 
-# Create a cursor object to execute SQL queries
-mycursor = mydb.cursor()
+    def connect(self):
+        self.db = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.db.cursor()
 
-# Prompt the user for input
-search_value = input("Enter the search value: ")
+    def execute_query(self, sql_query, query_params):
+        self.cursor.execute(sql_query, query_params)
+        return self.cursor.fetchall()
 
-# Ask the user what information they want to search for
-while True:
-    search_choice = input(""""What information do you want to search for?
+    def search_customers(self):
+        # Prompt the user for input
+        search_value = input("Enter the search value: ")
+
+        # Ask the user what information they want to search for
+        while True:
+            search_choice = input("""What information do you want to search for?
 enter
 '1' for ID,
 '2' for address,
 '3' for ID and address
 '4' for to show everthing
 what is your choice : """)
-    if search_choice == '1':
-        sql_query = "SELECT id FROM customers WHERE name = %s"
-        break
-    elif search_choice == '2':
-        sql_query = "SELECT address FROM customers WHERE name = %s"
-        break
-    elif search_choice == '3':
-        sql_query = "SELECT id, address FROM customers WHERE name = %s"
-        break
-    elif search_choice == '4':
-        sql_query = "SELECT * FROM customers WHERE name = %s"
-        break
-    else:
-        print("Invalid choice. Please enter '1', '2', '3' or '4'.")
+            if search_choice == '1':
+                sql_query = "SELECT id FROM customers WHERE name = %s"
+                break
+            elif search_choice == '2':
+                sql_query = "SELECT address FROM customers WHERE name = %s"
+                break
+            elif search_choice == '3':
+                sql_query = "SELECT id, address FROM customers WHERE name = %s"
+                break
+            elif search_choice == '4':
+                sql_query = "SELECT * FROM customers WHERE name = %s"
+                break
+            else:
+                print("Invalid choice. Please enter '1', '2', '3' or '4'.")
 
-# Define the parameter(s) for the query
-query_param = (search_value,)
+        # Define the parameter(s) for the query
+        query_params = (search_value,)
 
-# Execute the query with the parameter(s)
-mycursor.execute(sql_query, query_param)
+        # Execute the query with the parameter(s)
+        results = self.execute_query(sql_query, query_params)
 
-# Fetch all the matching rows
-results = mycursor.fetchall()
+        # Print the results
+        if len(results) > 0:
+            for row in results:
+                print(row)
+        else:
+            print("No results found.")
 
-# Print the results
-if len(results) > 0:
-    for row in results:
-        print(row)
-else:
-    print("No results found.")
+# Create an instance of the Database class and search for customers
+db = Database("localhost", "root", "Spysiddhu@#2004", "scrap")
+db.search_customers()
